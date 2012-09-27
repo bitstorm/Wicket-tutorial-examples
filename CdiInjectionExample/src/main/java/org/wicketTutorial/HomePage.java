5f32be696ffa101719053572cde2b64688bfe65e
andrea <andrea@andrea-GA-MA78GM-S2H>
2012-09-27 14:12:55 +0200

    Added CdiInjectionExample

---
+++ CdiInjectionExample/src/main/java/org/wicketTutorial/HomePage.java
@@ -0,0 +1,46 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wicketTutorial;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketTutorial.ejbBean.EnterpriseMessage;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private EnterpriseMessage enterpriseMessage;
+	
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+	
+		add(new Label("message", enterpriseMessage.message));
+    }
+	
+	public EnterpriseMessage getEnterpriseMessage() {
+		return enterpriseMessage;
+	}
+
+	public void setEnterpriseMessage(EnterpriseMessage enterpriseMessage) {
+		this.enterpriseMessage = enterpriseMessage;
+	}
+}
