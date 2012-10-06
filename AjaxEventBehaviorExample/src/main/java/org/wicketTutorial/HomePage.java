153adfee821b26c6c5d6ffcf8c9241d94d423911
andrea <andrea@andrea-GA-MA78GM-S2H>
2012-10-06 20:26:05 +0200

    Added AjaxEventBehaviorExample

---
+++ AjaxEventBehaviorExample/src/main/java/org/wicketTutorial/HomePage.java
@@ -0,0 +1,70 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wicketTutorial;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+    public HomePage(final PageParameters parameters) {
+		super(parameters);
+	
+		final ClickCounterLabel clickCounterLabel = 
+				new ClickCounterLabel("clickCounterLabel", "Click on me!");
+		final Label clickCounter 
+				= new Label("clickCounter", new PropertyModel(clickCounterLabel, "clickCounter"));
+		
+		
+		clickCounterLabel.setOutputMarkupId(true);
+		clickCounterLabel.add(new AjaxEventBehavior("click"){
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				clickCounterLabel.clickCounter++;
+				target.add(clickCounter);
+			}			
+		});
+		
+		add(clickCounterLabel);
+		add(clickCounter.setOutputMarkupId(true));		
+    }
+}
+
+class ClickCounterLabel extends Label{
+	public int clickCounter;	
+
+	public ClickCounterLabel(String id) {
+		super(id);
+	}
+
+	public ClickCounterLabel(String id, IModel<?> model) {
+		super(id, model);
+	}
+
+	public ClickCounterLabel(String id, String label) {
+		super(id, label);		
+	}
+	
+}
