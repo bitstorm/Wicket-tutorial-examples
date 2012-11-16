9b2c65e2f72b11529b7fb39f971839ebc8732deb
andrea <andrea@andrea-GA-MA78GM-S2H>
2012-11-16 14:21:28 +0100

    Work in progress...

--- CheckBoxAjaxTree/src/main/java/org/wicketTutorial/HomePage.java
+++ CheckBoxAjaxTree/src/main/java/org/wicketTutorial/HomePage.java
@@ -1,18 +1,60 @@
 package org.wicketTutorial;
 
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+import org.apache.wicket.Component;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.extensions.markup.html.repeater.tree.NestedTree;
+import org.apache.wicket.extensions.markup.html.repeater.tree.content.CheckedFolder;
+import org.apache.wicket.extensions.markup.html.repeater.util.TreeModelProvider;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
     public HomePage(final PageParameters parameters) {
-	super(parameters);
-
-	add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-        // TODO Add your page's components here
-
+		super(parameters);
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Cities of Europe");
+		
+		addNodes(addNode(root, "Italy"), "Venice", "Milan", "Florence");
+		
+		DefaultTreeModel treeModel = new DefaultTreeModel(root);
+		TreeModelProvider<String> modelProvider = new TreeModelProvider<String>(treeModel) {
+			
+			@Override
+			public IModel<String> model(String object) {
+				return Model.of(object);
+			}
+		};
+		
+		NestedTree<String> tree = new NestedTree<String>("tree", modelProvider)
+	    {
+	        private static final long serialVersionUID = 1L;
+	        
+	        @Override
+	        protected Component newContentComponent(String id, IModel<String> model)
+	        {
+	            return new CheckedFolder<String>(id, this, model);
+	        }
+	    };
+	    
+	    add(tree);
+    }
+    
+    public static void addNodes(DefaultMutableTreeNode parent, String... childrenNode){
+    	for (int i = 0; i < childrenNode.length; i++) {
+			addNode(parent, childrenNode[i]);
+		}
+    }
+    
+    public static DefaultMutableTreeNode addNode(DefaultMutableTreeNode parent, String childNode){
+    	DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(childNode);    	
+    	parent.add(newNode);
+    	
+    	return newNode;
     }
 }
