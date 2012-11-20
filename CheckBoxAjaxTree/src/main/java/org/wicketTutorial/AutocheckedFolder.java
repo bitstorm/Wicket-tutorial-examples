d2fcf20e15d46e5e62cde7f19dc20c53b8db63f4
bitstorm <an.delbene @gmail.com>
2012-11-20 12:53:01 +0100

    Work in progress...

--- CheckBoxAjaxTree/src/main/java/org/wicketTutorial/AutocheckedFolder.java
+++ CheckBoxAjaxTree/src/main/java/org/wicketTutorial/AutocheckedFolder.java
@@ -1,5 +1,6 @@
 package org.wicketTutorial;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.markup.html.repeater.tree.AbstractTree;
@@ -12,28 +13,39 @@ import org.apache.wicket.model.PropertyModel;
 public class AutocheckedFolder<T> extends CheckedFolder<T> {
 
 	private ITreeProvider<T> treeProvider;
-	private boolean selected;
+	private boolean nodeChecked;
+	private IModel<Set<T>> treeModel;
 	
 	public AutocheckedFolder(String id, AbstractTree<T> tree, IModel<T> model) {
 		super(id, tree, model);	
 		this.treeProvider = tree.getProvider();
+		this.treeModel = tree.getModel();
 	}
 	
 	@Override
 	protected IModel<Boolean> newCheckBoxModel(IModel<T> model) {
-		return new PropertyModel<Boolean>(this, "selected");
+		return new PropertyModel<Boolean>(this, "nodeChecked");
 	}
 	
 	@Override
 	protected void onUpdate(AjaxRequestTarget target) {
 		super.onUpdate(target);
-		System.out.println("let's see!");
 		
-		if(isSelected())
+		if(nodeChecked)
 		{
 			T node = getModelObject();
-			Iterator<? extends T> childrenNodes = treeProvider.getChildren(node);
-			System.out.println("SELECTED!");
+			addRemoveSubNodes(node);
 		}
+	}
+	
+	private void addRemoveSubNodes(T node) {
+		Iterator<? extends T> childrenNodes = treeProvider.getChildren(node);
+		
+		while (childrenNodes.hasNext()) {
+			T childNode = childrenNodes.next();
+			addRemoveSubNodes(childNode);
+		}
+		
+		treeModel.getObject().add(node);
 	}
 }
