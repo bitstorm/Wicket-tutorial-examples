febb6702400f9cebab326522ddef188ce92e7af2
bitstorm <an.delbene @gmail.com>
2012-11-20 18:11:29 +0100

    Almost done with CheckBoxAjaxTree...

--- CheckBoxAjaxTree/src/main/java/org/wicketTutorial/AutocheckedFolder.java
+++ CheckBoxAjaxTree/src/main/java/org/wicketTutorial/AutocheckedFolder.java
@@ -6,6 +6,8 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.markup.html.repeater.tree.AbstractTree;
 import org.apache.wicket.extensions.markup.html.repeater.tree.ITreeProvider;
 import org.apache.wicket.extensions.markup.html.repeater.tree.content.CheckedFolder;
+import org.apache.wicket.extensions.markup.html.repeater.tree.content.Folder;
+import org.apache.wicket.extensions.markup.html.repeater.tree.nested.BranchItem;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 
@@ -13,7 +15,7 @@ import org.apache.wicket.model.PropertyModel;
 public class AutocheckedFolder<T> extends CheckedFolder<T> {
 
 	private ITreeProvider<T> treeProvider;
-	private Boolean nodeChecked;
+	private Boolean nodeChecked = false;
 	private IModel<Set<T>> checkedNodes;
 	
 	public AutocheckedFolder(String id, AbstractTree<T> tree, 
@@ -33,13 +35,18 @@ public class AutocheckedFolder<T> extends CheckedFolder<T> {
 	@Override
 	protected void onUpdate(AjaxRequestTarget target) {
 		super.onUpdate(target);
-		
 		T node = getModelObject();
+		
 		addRemoveSubNodes(node, nodeChecked);				
+		addRemoveAncestorNodes(node, nodeChecked);
 		
 		target.appendJavaScript(";$('#" + getMarkupId() + 
 				"').closest('.tree-node').siblings('.tree-subtree')" +
 				".find('input[type=checkbox]').prop('checked', " + nodeChecked + ");");
+		
+		for (T curnode : checkedNodes.getObject()) {
+			System.out.println(curnode);
+		}
 	}
 	
 	private void addRemoveSubNodes(T node, Boolean nodeStatus) {
@@ -56,6 +63,43 @@ public class AutocheckedFolder<T> extends CheckedFolder<T> {
 			checkedNodes.getObject().remove(node);
 	}
 	
+	private void addRemoveAncestorNodes(T node, Boolean nodeStatus) {
+		BranchItem currentConatiner = findParent(BranchItem.class);
+		
+		if(currentConatiner == null) return;
+		
+		BranchItem ancestor = currentConatiner.findParent(BranchItem.class);
+		
+		while(ancestor != null)
+		{
+			T nodeObject = (T) ancestor.getDefaultModelObject();
+			
+			if(nodeStatus)
+			{
+				checkedNodes.getObject().add(nodeObject);
+			}
+			else
+			{
+				if(!hasNodeCheckedChildren(nodeObject))
+					checkedNodes.getObject().remove(nodeObject);				
+			}
+			
+			ancestor = ancestor.findParent(BranchItem.class);
+		}
+	}
+
+	private boolean hasNodeCheckedChildren(T nodeObject) {
+		Iterator<? extends T> children = treeProvider.getChildren(nodeObject);
+		
+		while (children.hasNext()) {
+			T child = children.next();
+			if(checkedNodes.getObject().contains(child))
+				return true;
+		}
+		
+		return false;
+	}
+
 	class CheckModel implements IModel<Boolean>{		
 		private T node;
 
