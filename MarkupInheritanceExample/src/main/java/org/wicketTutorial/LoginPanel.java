d0dc2c069ccd08cacb584fd203b9d4358f068158
andrea <andrea@andrea-GA-MA78GM-S2H>
2012-11-22 19:18:24 +0100

    Added UML class diagram (for AmaterasUML)

--- MarkupInheritanceExample/src/main/java/org/wicketTutorial/LoginPanel.java
+++ MarkupInheritanceExample/src/main/java/org/wicketTutorial/LoginPanel.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.wicketTutorial;;
+package org.wicketTutorial;
 
 import org.apache.wicket.MetaDataKey;
 
