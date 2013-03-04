6dd86ddb11e66e619aa527dd78b2984adb8147dc
bitstorm <an.delbene@gmail.com>
2013-03-04 12:22:46 +0100

    Added project LocalizedChoicesExample

--- LocalizedChoicesExample/src/main/java/org/wicketTutorial/HomePage.java
+++ LocalizedChoicesExample/src/main/java/org/wicketTutorial/HomePage.java
@@ -16,14 +16,49 @@
  */
 package org.wicketTutorial;
 
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.wicket.Session;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.Model;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
+
+		List<Locale> locales = Arrays.asList(Locale.ENGLISH, Locale.ITALIAN, Locale.GERMAN);
+		List<String> colors = Arrays.asList("green", "red", "blue", "yellow");
+		
+		final DropDownChoice<Locale> changeLocale = new DropDownChoice<Locale>("changeLocale", new Model<Locale>(), locales);
+		
+		StatelessForm form = new StatelessForm("form"){
+			@Override
+			protected void onSubmit() {
+				Session.get().setLocale(changeLocale.getModelObject());
+			}
+		};		
+		
+		DropDownChoice<String> selectColor = new DropDownChoice<String>("selectColor", new Model<String>(), colors){
+			@Override
+			protected boolean localizeDisplayValues() {
+				return true;
+			}
+		};
+		
+		form.add(selectColor);
+		
+		setStatelessHint(true);
+		add(form.add(changeLocale));
+
     }
 }
--- LocalizedChoicesExample/src/test/java/org/wicketTutorial/TestHomePage.java
+++ LocalizedChoicesExample/src/test/java/org/wicketTutorial/TestHomePage.java
@@ -16,11 +16,7 @@
  */
 package org.wicketTutorial;
 
-import java.util.List;
-
-import org.apache.wicket.util.tester.TagTester;
 import org.apache.wicket.util.tester.WicketTester;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -38,21 +34,12 @@ public class TestHomePage
 	}
 
 	@Test
-	public void homePageMarkupTest()
+	public void homepageRendersSuccessfully()
 	{
 		//start and render the test page
 		tester.startPage(HomePage.class);
-		//retrieve response markup
-		String responseTxt = tester.getLastResponse().getDocument();
-		
-		TagTester tagTester = TagTester.createTagByAttribute(responseTxt, "class", "myClass"); 
-		
-		Assert.assertNotNull(tagTester);
-		Assert.assertEquals("span", tagTester.getName());		
-		
-		List<TagTester> tagTesterList = TagTester.createTagsByAttribute(responseTxt, "class", "myClass", 
-																	false);
-		
-		Assert.assertEquals(2, tagTesterList.size());
+
+		//assert rendered page class
+		tester.assertRenderedPage(HomePage.class);
 	}
 }
