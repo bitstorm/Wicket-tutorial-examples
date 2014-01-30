963013b28b41ff95db5b18748c8b10f025bcd051
Andrea Del Bene <an.delbene@gmail.com>
2014-01-30 13:52:43 +0100

    Example code.

--- ScriptInsideBody/src/main/java/org/wicketTutorial/HomePage.java
+++ ScriptInsideBody/src/main/java/org/wicketTutorial/HomePage.java
@@ -1,19 +1,30 @@
 package org.wicketTutorial;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnEventHeaderItem;
 import org.apache.wicket.markup.head.filter.HeaderResponseContainer;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.PackageResourceReference;
 
 public class HomePage extends WebPage {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
-		super(parameters);
+    public HomePage(final PageParameters parameters) {
+        super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+        add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
-		add(new HeaderResponseContainer("footer-container", "footer-container"));
+        add(new HeaderResponseContainer("footer-container", "footer-container"));
+    }
 
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(getClass(),
+                "javasciptLibrary.js")));
+
+        response.render(OnEventHeaderItem.forScript("'logo'", "click", "alert('Clicked me!')"));
     }
 }
---
+++ ScriptInsideBody/src/main/java/org/wicketTutorial/javasciptLibrary.js
@@ -0,0 +1,3 @@
+/**
+ * Empty js file
+ */
