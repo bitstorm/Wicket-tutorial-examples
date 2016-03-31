d9db53ee61b90b14a41ec67d7cefb9e4fe7b68b6
Andrea Del Bene <andrea.delbene@innoteam.it>
2016-03-31 16:58:12 +0200

    Almost all examples added

--- StarterExamples/src/main/java/org/wicketTutorial/HomePage.java
+++ StarterExamples/src/main/java/org/wicketTutorial/HomePage.java
@@ -17,7 +17,7 @@
 package org.wicketTutorial;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.link.StatelessLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
@@ -26,7 +26,7 @@ public class HomePage extends WebPage {
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 		
-		add(new Link<Void>("linkToHttps")
+		add(new StatelessLink<Void>("linkToHttps")
 		{
 			@Override
 			public void onClick()
