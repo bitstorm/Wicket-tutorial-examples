b254058847c790d410e3a075ce8671b459d71605
Andrea Del Bene <andrea.delbene@innoteam.it>
2017-04-13 17:40:58 +0200

    Introduced Bootstrap common module

---
+++ BootstrapCommon/src/main/java/org/wicketTutorial/commons/bootstrap/BootstrapApp.java
@@ -0,0 +1,40 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wicketTutorial.commons.bootstrap;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.BootstrapSettings;
+import de.agilecoders.wicket.core.settings.IBootstrapSettings;
+import de.agilecoders.wicket.core.settings.ThemeProvider;
+import de.agilecoders.wicket.themes.markup.html.bootswatch.BootswatchTheme;
+import de.agilecoders.wicket.themes.markup.html.bootswatch.BootswatchThemeProvider;
+
+public abstract class BootstrapApp extends WebApplication {
+
+	@Override
+	protected void init() {
+		final IBootstrapSettings settings = new BootstrapSettings();
+        final ThemeProvider themeProvider = new BootswatchThemeProvider(BootswatchTheme.United);
+
+        settings.setThemeProvider(themeProvider);
+        settings.setDeferJavascript(true);
+        
+        Bootstrap.install(this, settings);
+	}
+}
---
+++ BootstrapCommon/src/test/java/org/wicketTutorial/commons/bootstrap/TestHomePage.java
@@ -0,0 +1,21 @@
+package org.wicketTutorial.commons.bootstrap;
+
+import org.junit.Test;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestHomePage
+{
+	
+
+	@Test
+	public void homepageRendersSuccessfully()
+	{
+//		//start and render the test page
+//		tester.startPage(HomePage.class);
+//
+//		//assert rendered page class
+//		tester.assertRenderedPage(HomePage.class);
+	}
+}
